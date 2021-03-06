c044d6ce7db459ad7b71a360bb9248425b1bb26a
Fábio Borges de Oliveira Vilela <fabioborgesoliveiravilela@gmail.com>
2021-03-06 18:07:57 -0300

    Create ProposalOutputWriter.java

---
+++ src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/writer/ProposalOutputWriter.java
@@ -0,0 +1,9 @@
+package br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique.writer;
+
+public class ProposalOutputWriter {
+
+    public String write(){
+
+        return null;
+    }
+}
