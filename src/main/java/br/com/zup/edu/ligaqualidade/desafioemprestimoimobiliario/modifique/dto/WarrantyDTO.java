3f15b63e7cb1b826a650cfda57f3342212f2d246
Fábio Borges de Oliveira Vilela <fabioborgesoliveiravilela@gmail.com>
2021-03-06 18:03:13 -0300

    Update WarrantyDTO.java

--- src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/dto/WarrantyDTO.java
+++ src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/dto/WarrantyDTO.java
@@ -2,46 +2,41 @@ package br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique.dto;
 
 import java.math.BigDecimal;
 
-public class WarrantyDTO extends WarrantyGenericDTO {
+public class WarrantyDTO extends GenericInstanceDTO {
 
-	private BigDecimal value;
+    private final String id;
 
-	private String province;
+    private final BigDecimal value;
 
-	public WarrantyDTO() {
-		super();
-	}
+    private final String province;
 
-	public WarrantyDTO(String eventId, String eventSchema, String eventAction, //
-			String eventTime, String proposalId,//
-			String warrantyId, BigDecimal warrantyValue, String warrantyProvince) {
-		super(eventId, eventSchema, eventAction, eventTime, proposalId, warrantyId);
-		this.value = warrantyValue;
-		this.province = warrantyProvince;
-	}
+    public WarrantyDTO(String eventId, String eventSchema, String eventAction,
+                       String eventTime, String proposalId,
+                       String warrantyId, BigDecimal warrantyValue, String warrantyProvince) {
+        super(eventId, eventSchema, eventAction, eventTime, proposalId);
+        this.id = warrantyId;
+        this.value = warrantyValue;
+        this.province = warrantyProvince;
+    }
 
-	public BigDecimal getValue() {
-		return value;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setValue(BigDecimal warrantyValue) {
-		this.value = warrantyValue;
-	}
+    public BigDecimal getValue() {
+        return value;
+    }
 
-	public String getProvince() {
-		return province;
-	}
+    public String getProvince() {
+        return province;
+    }
 
-	public void setProvince(String warrantyProvince) {
-		this.province = warrantyProvince;
-	}
-
-	@Override
-	public String toString() {
-		return "WarrantyDTO [value=" + value + ", province=" + province + ", getId()=" + getId() + ", getEventId()="
-				+ getEventId() + ", getEventSchema()=" + getEventSchema() + ", getEventAction()=" + getEventAction()
-				+ ", getEventTime()=" + getEventTime() + ", getProposalId()=" + getProposalId() + ", toString()="
-				+ super.toString() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + "]";
-	}
+    @Override
+    public String toString() {
+        return "WarrantyDTO [value=" + value + ", province=" + province + ", getId()=" + getId() + ", getEventId()="
+                + getEventId() + ", getEventSchema()=" + getEventSchema() + ", getEventAction()=" + getEventAction()
+                + ", getEventTime()=" + getEventTime() + ", getProposalId()=" + getProposalId() + ", toString()="
+                + super.toString() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + "]";
+    }
 
 }
