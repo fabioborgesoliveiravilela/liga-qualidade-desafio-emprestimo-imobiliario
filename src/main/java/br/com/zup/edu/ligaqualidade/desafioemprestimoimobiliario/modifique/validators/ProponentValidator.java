fa8106a6a884787a763f8729230b718651a32510
Fábio Borges de Oliveira Vilela <fabioborgesoliveiravilela@gmail.com>
2021-03-06 18:05:36 -0300

    Update ProponentValidator.java

--- src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/validators/ProponentValidator.java
+++ src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/validators/ProponentValidator.java
@@ -1,51 +1,45 @@
 package br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique.validators;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique.dto.GenericInstanceDTO;
 import br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique.dto.ProponentDTO;
+import br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique.dto.ProposalDTO;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public class ProponentValidator {
 
-	public List<String> validate(List<GenericInstanceDTO> list) {
-		if (list == null || list.isEmpty()) {
-			return new ArrayList<>();
-		}
-		List<ProponentDTO> proponents = new ArrayList<>();
-		list.forEach(ins -> proponents.add((ProponentDTO) ins));
-		return this.validateList(proponents);
-	}
+    public void validate(ProposalDTO proposal) {
 
-	private List<String> validateList(List<ProponentDTO> proponents) {
+        List<ProponentDTO> proponents = proposal.getProponents();
 
-		Map<String, List<ProponentDTO>> groupedProponents = groupingProponentsByProposalId(proponents);
-		List<String> validatedProponents = new ArrayList<>();
-		for (String proposalId : groupedProponents.keySet()) {
-			var proponentsById = groupedProponents.get(proposalId);
+        if (proponents.size() < 2) {
+            throw new IllegalArgumentException("Deve haver no mínimo 2 proponentes por proposta");
+        }
 
-			Optional<ProponentDTO> optionalProponent = findMainProponent(proponentsById);
-			if (hasMoreThan1ProponentAndMainProponentAndAgeGreaterThan18(proponentsById, optionalProponent)) {
-				validatedProponents.add(proposalId);
-			}
-		}
+        if (proponents.stream().filter(it -> it.isMain()).count() > 1) {
+            throw new IllegalArgumentException("Deve haver exatamente 1 proponente principal por proposta");
+        }
 
-		return validatedProponents;
-	}
+        if (proponents.stream().filter(it -> it.getAge() < 18).count() > 1) {
+            throw new IllegalArgumentException("Todos os proponentes devem ser maiores de 18 anos");
+        }
 
-	private boolean hasMoreThan1ProponentAndMainProponentAndAgeGreaterThan18(List<ProponentDTO> proponentsById,
-			Optional<ProponentDTO> optionalProponent) {
-		return proponentsById.size() > 1 && optionalProponent.isPresent() && optionalProponent.get().getAge() >= 18;
-	}
+        proponents.stream().filter(it -> it.isMain()).forEach(it ->
+                {
+                    BigDecimal monthlyIncome = it.getMonthlyIncome();
+                    Integer age = it.getAge();
 
-	private Optional<ProponentDTO> findMainProponent(List<ProponentDTO> proponentsById) {
-		return proponentsById.stream().filter(p -> p.isMain()).findFirst();
-	}
+                    if (age >= 18 && age <= 24 && monthlyIncome.multiply(BigDecimal.valueOf(4)).compareTo(proposal.getInstallmentValue()) >= 1) {
 
-	private Map<String, List<ProponentDTO>> groupingProponentsByProposalId(List<ProponentDTO> proponents) {
-		return proponents.stream().collect(Collectors.groupingBy(p -> p.getProposalId()));
-	}
+                        throw new IllegalArgumentException("A renda do proponente principal deve ser pelo menos 4 vezes o valor da parcela do empréstimo");
+                    } else if (age >= 25 && age <= 50 && monthlyIncome.multiply(BigDecimal.valueOf(3)).compareTo(proposal.getInstallmentValue()) >= 1) {
+
+                        throw new IllegalArgumentException("A renda do proponente principal deve ser pelo menos 3 vezes o valor da parcela do empréstimo");
+                    } else if (monthlyIncome.multiply(BigDecimal.valueOf(2)).compareTo(proposal.getInstallmentValue()) >= 1) {
+
+                        throw new IllegalArgumentException("A renda do proponente principal deve ser pelo menos 2 vezes o valor da parcela do empréstimo");
+                    }
+                }
+        );
+    }
 }
